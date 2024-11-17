b3ba075fd6fc2c32f160da68be69e260c0392588
gigaxucishvili2007 <158300399+gigaxucishvili2007@users.noreply.github.com>
2024-11-17 17:40:25 +0400

    commit

---
+++ day 01/homework/homeworkN1.py
@@ -0,0 +1,43 @@
+"""
+boolean values
+a = True
+b = False
+print(a or b) # answer: True
+print(a and b) # answer: False
+
+პირველი დავალება შესრულებულია✅
+"""
+"""
+x = True
+y = False
+print(x or y) # answer: True
+print(x and y) # answer: False
+
+მეორე დავალება შესრულებულია✅
+"""
+
+"""
+print(10 > 2) answer: True
+print(12 > 15)answer: False
+მესამე დავალება შესრულებულია✅
+"""
+
+"""
+ინფუთზეც გავაკეთე რამოდენიმე მაგალითი:
+
+სათამაშოს მოგება:🦖
+
+"""
+num = int(input('enter any number'))
+if num > 10:
+    print("shen moige dinozavri")
+elif num < 10:
+    print("ver moige samwuxarod")
+elif num != 2:
+    print("kidev scade")
+elif num != 3:
+    print("kidev erti shansi gakvs")
+else:
+    print("sxvadros scade")
+
+
