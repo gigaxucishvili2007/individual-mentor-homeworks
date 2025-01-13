66804796a842186755ff2c9db8d9a6dd5728f27c
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2025-01-13 04:24:13 -0800

    commit

--- day 06/homework/hw.py
+++ day 06/homework/hw.py
@@ -1,1 +1,11 @@
-# Codewars:
+# bmi calculator - Codewars:
+def bmi(height,weight):
+    bm_calculate = weight/(height **2)
+    if bm_calculate <= 18.5:
+        return "Underweight"
+    elif bm_calculate <= 25.0:
+        return "Normal"
+    elif bm_calculate <= 30.0:
+        return "Overweight"
+    elif bm_calculate > 30:
+        return "Obese"
