3a3cfc8ed1cb9b06c91026d562883c0cbe4d3cf4
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2025-01-24 07:18:29 -0800

    commit

---
+++ day 07/homework/worst_case.py
@@ -0,0 +1,24 @@
+"""
+def findmax(arr):
+    maximal = arr[0]
+    for i in range (1, len(arr)):
+        if arr[i] > maximal:
+            maximal = arr[i]
+    return maximal
+
+print(findmax([2, 3, 4, 6, 8]))
+"""
+# დაწერე ამის worst_case
+
+def findmax(arr):
+    maximal = arr [0]
+    for i in range (1, len(arr)):
+        if arr[i] > maximal:
+            maximal = arr [i]
+        elif arr != [i]:
+            maximal != [i]
+        elif arr == [i]:
+            arr == [i]
+    return maximal
+findmax([0])
+
